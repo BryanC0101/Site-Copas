343c89a6cffc9036915a458360dceafed84ab05e
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-08-12 10:21:07 -0300

    news searching

--- noticias/portalnews.js
+++ noticias/portalnews.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
 
+
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("ativo");
 
@@ -9,4 +10,20 @@ hamburger.addEventListener("click", () => {
     } else {
         hamburger.textContent = "☰";
     }
+});
+
+const pesquisa = document.querySelector("#pesquisa-noticia");
+const noticias = document.querySelectorAll(".link-noticia");
+
+
+pesquisa.addEventListener("input", () => {
+    const texto = pesquisa.value.toLowerCase();
+    noticias.forEach(noticia => {
+        const titulo = noticia.querySelector("h1").textContent.toLowerCase();
+        if (titulo.includes(texto)) {
+            noticia.style.display = "";
+        } else {
+            noticia.style.display = "none";
+        }
+    })
 });
