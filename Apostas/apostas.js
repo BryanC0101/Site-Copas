5183ebecfb644d4150eecacd5b1b2c2bc4155d71
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-09-09 00:48:52 -0300

    ...

---
+++ Apostas/apostas.js
@@ -0,0 +1,15 @@
+const tabelaOdds = [
+    {
+        selecao: "Vietna",
+        odd: 2.40
+    },
+    {
+        selecao: "Macedônia do Norte",
+        odd: 5.80
+    },
+    {
+        selecao: "França",
+        odd: 5.60
+    }
+    
+]
